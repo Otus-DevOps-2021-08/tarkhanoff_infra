afe96f415cd7b9355247be0bf7e968c5a815abd1
Denis Tarkhanov <dtarhanov@mail.ru>
2021-10-12 15:32:09 +0300

    Install mongodb-org-shell before mongodb-org

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-sudo apt-get install apt-transport-https ca-certificates
+sudo apt-get install apt-utils apt-transport-https ca-certificates
 
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
 sudo apt-get update
+sudo apt-get install -y mongodb-org-shell
 sudo apt-get install -y mongodb-org
 
 sudo systemctl start mongod
